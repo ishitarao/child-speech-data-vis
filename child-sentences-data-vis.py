25ec9e2e5e66d5344cfa6aaa9ee62b3968e1d9ac
ishitarao <ir5@illinois.edu>
2020-04-18 18:43:37 -0500

    Increase the code's expandability by removing a lot of hard coding; refine the detection of -ing and -ed words; refine the detection of subjects and verbs by using sentence codes

--- child-sentences-data-vis.py
+++ child-sentences-data-vis.py
@@ -13,16 +13,12 @@ nlp = spacy.load('en_core_web_sm')
 
 
 # checks if the sentence is an Active Declarative Sentence
-def is_ads(sentence):
-    doc = nlp(sentence)
+def is_ads(doc):
     is_subj = False
     is_verb = False
     is_obj = False
 
     for token in doc:
-        # bad hard coding
-        if token.text == 'Pooh':
-            is_subj = True
         if token.dep_ == 'nsubj':
             is_subj = True
         if token.pos_ == 'VERB' or token.pos_ == 'AUX':
@@ -30,7 +26,9 @@ def is_ads(sentence):
                 return False
             else:
                 is_verb = True
-        if 'obj' in token.dep_:
+        if token.dep_ == 'advmod' and is_verb is False:
+            continue
+        if 'obj' in token.dep_ or 'advmod' == token.dep_:
             if is_verb is False:
                 return False
             else:
@@ -47,17 +45,27 @@ def is_copula(sentence):
 
 # Cleans the sentence of any mazes and codes
 def clean_sentence(sentence):
+    if re.search('^C', sentence) is None:
+        return 'Not a child sentence :('
     # Clean the C at the beginning of the sentence
     sentence = re.sub("^C ", '', sentence)
 
     # Clean mazes
-    sentence = re.sub('\\(.+\\)\\s', '', sentence)
+    sentence = re.sub('\\(.+?\\)\\s', '', sentence)
 
     # Clean pipes (the word before the pipe)
     sentence = re.sub('\\s\\w*?\\|', ' ', sentence)
 
-    # Clean slashes (this doesn't work, will probably change it
-    sentence = re.sub('/\\*.*?\\s|\\.', ' ', sentence)
+    # Clean slashes
+    sentence = sentence.replace('/*3', '')
+    stops = ['p', 'b', 't', 'd']
+    for ch in stops:
+        sentence = sentence.replace(ch + '/ing', ch + ch + 'ing')
+        sentence = sentence.replace(ch + '/ed', ch + ch + 'ed')
+
+    sentence = sentence.replace('/ing', 'ing')
+    sentence = sentence.replace('/ed', 'ed')
+    sentence = sentence.replace('/s', 's')
 
     # Clean asterisks (the entire word)
     sentence = re.sub('\\*.*?\\s', '', sentence)
@@ -71,6 +79,9 @@ def clean_sentence(sentence):
 
     # Clean brackets [ ]
     sentence = re.sub('\\[.*?\\]', '', sentence)
+
+    # Clean any accidental double spaces
+    sentence = sentence.replace('  ', ' ')
 
     return sentence
 
@@ -91,32 +102,44 @@ def get_lemma_with_pos(doc, pos):
     return None
 
 
-# Parses through the given sentence and returns a dict containing the subject and the verb
-def parse_sentence(sentence):
-    doc = nlp(sentence)
-    subject = get_token_with_dep(doc, 'nsubj')
-    # bad hard coding
-    if subject is None:
-        subject = 'Pooh'
+def find_verb_uncoded(doc):
     verb = get_lemma_with_pos(doc, 'VERB')
     if verb is None:
         verb = get_lemma_with_pos(doc, 'AUX')
-
-    # obj = get_token_with_dep(doc, 'dobj')
-    # if obj is None:
-    #     obj = get_token_with_dep(doc, 'obj')
-    #
-    # svo = {'subject': subject, 'verb': verb, 'object': obj}
-
-    # WARNING: Really bad hardcoding incoming
-    if verb == 'nee':
-        verb = 'need'
-    if verb == 'will':
-        verb = 'get'
-    sv = {'subject': subject, 'verb': verb}
-    return sv
+    return verb
 
 
+# Uses codes
+def find_verb_coded(sentence):
+    verb_codes = ['[0]', '/ed', '/ing', '[i3:o]']
+    needed_code = ''
+    for code in verb_codes:
+        if code in sentence:
+            needed_code = code
+            break
+
+    if needed_code == '':
+        return ':('
+
+    words_list = sentence.split()
+    for word in words_list:
+        if needed_code in word:
+            return word.split(needed_code)[0]
+
+    return ':('
+
+
+# Uses codes
+def find_subject(sentence):
+    words_list = sentence.split()
+    for word in words_list:
+        if re.search('\\[SV:\\dP?\\]', word) is not None:
+            return word.split('[')[0].replace('/', '')
+
+    return ':('
+
+
+# Returns a nested dict given a base type and number of nests
 def nested_dict(n, type):
     if n == 1:
         return defaultdict(type)
@@ -143,15 +166,36 @@ def to_list(array):
     return sentences_list
 
 
-def read_file(loc):
-    dataframe = pandas.read_excel(loc)
-    dataframe = dataframe.iloc[6:] # 6
+def find_given_child_sentences(dataframe, child_id):
+    # find starting point
+    count = 0
+    for value in dataframe.iloc[:, 0]:
+        count += 1
+        if value == child_id:
+            dataframe = dataframe.iloc[count:]
+            break
+
+    # find ending point
+    count = 0
+    for value in dataframe.iloc[:, 0]:
+        count += 1
+        if value == 'Total Frequency':
+            dataframe = dataframe.iloc[:count]
+            break
+
+    # remove the unneeded columns, duplicates, and NaN values
     dataframe = dataframe.drop(dataframe.columns[[0, 2]], axis=1)
-    dataframe = dataframe.drop_duplicates()
+    dataframe = dataframe.drop_duplicates().dropna()
+
+    return dataframe
+
+
+def read_file(loc, child_id):
+    dataframe = pandas.read_excel(loc)
+    dataframe = find_given_child_sentences(dataframe, child_id)
     sentences_array = []
 
-    # add the first x sentences of the excel file to work with
-    for (cname, columnData) in dataframe.head(92).iteritems():
+    for (cname, columnData) in dataframe.iteritems():
         sentences_array.append(columnData.values)
 
     return to_list(sentences_array)
@@ -161,31 +205,44 @@ def classify_text(sentences_list):
     # an array containing SV dicts
     sv_list = []
     for sentence in sentences_list:
-        sentence = clean_sentence(sentence)
-        if is_copula(sentence) or not is_ads(sentence):
+        if is_copula(sentence):
             continue
 
-        sv_list.append(parse_sentence(sentence))
-        # sv = parse_sentence(sentence)
-        # if not sv_list.__contains__(sv):
-        #     sv_list.append(sv)
-        # else:
-        #     continue
+        subject = find_subject(sentence)
+        if subject == ':(':
+            continue
+
+        verb = find_verb_coded(sentence)
+
+        sentence = clean_sentence(sentence)
+        doc = nlp(sentence)
+        if not is_ads(doc):
+            print(doc)
+            continue
+
+        if verb == ':(':
+            verb = find_verb_uncoded(doc)
+
+        sv_list.append({'subject': subject, 'verb': verb})
 
     sv_count = count_subject_verbs(sv_list)
     df = pandas.DataFrame.from_dict(sv_count).fillna(0)
     return df
 
 
-def create_graph(df):
+def create_graph(df, child_id):
     sns.heatmap(df, annot=True, fmt="g", cmap='viridis').set(xlabel='Subject', ylabel='Verb')
-    # plt.savefig('Child-sentence-Heatmap.png', dpi=400)
+
+    # uncomment this line if you want to save the graph to file:
+    # plt.savefig(child_id + '_heatmap.png', dpi=400)
     plt.show()
 
 # ----------------------------------------------End function definitions---------------------------------------------- #
 
 
-location = "/Users/ishita/PycharmProjects/APL-child-sentences-data-vis/data/childsentences.xlsx"
-sentences = read_file(location)
+location = "/Users/ishita/PycharmProjects/sv-diversity-data-vis/data/childsentences.xlsx"
+child_id = 'JTGTP44B 30P.SLT'
+sentences = read_file(location, child_id)
 data_frame = classify_text(sentences)
-create_graph(data_frame)
+print(data_frame)
+create_graph(data_frame, child_id)
